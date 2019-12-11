b3c697421187a5efec475e87965e1dd3ae6dc61e
Grégoire Seux <g.seux@criteo.com>
2019-12-11 06:19:16 +0100

    First part day11
    Change-Id: I155d9b9aced62cd00a3aa37710e32d3aeccf3788

---
+++ day11.rb
@@ -0,0 +1,268 @@
+#!/usr/bin/env ruby
+
+input = DATA.read.split(',').map(&:to_i)
+input_copy = input.dup
+
+def debug(s)
+  puts s if ENV['DEBUG']
+end
+
+def debug2(s)
+  puts s if ENV['DEBUG2']
+end
+
+def read_param(param_mode, input, index, relative_base)
+  case param_mode
+  when 0 # position mode
+    debug "Reading param at index #{index} in position mode, points to index #{input[index]}, value #{input[input[index]]}"
+    input[input[index]]
+  when 1
+    debug "Reading param at index #{index} in immediate mode, value #{input[index]}"
+    input[index]
+  when 2
+    debug "Reading param at index #{index} in relative mode, points to index #{input[relative_base + input[index]]}, value #{input[input[relative_base + input[index]]]}"
+    input[relative_base + input[index]]
+  else
+    raise "Unknown param_mode #{param_mode}"
+  end
+end
+
+def write_param(param_mode, input, index, relative_base, value)
+  case param_mode
+  when 0
+    debug "Writing value #{value} in position mode at index #{index}, points to index #{input[index]}, previous value #{input[input[index]]}"
+    input[input[index]] = value
+  when 1
+    raise "Cannot use immediate mode to write!"
+  when 2
+    debug "Writing value #{value} in relative mode at index #{index}, points to index #{input[index] + relative_base}, previous value #{input[input[index] + relative_base]}"
+    input[input[index] + relative_base] = value
+  else
+    raise "Unknown param_mode #{param_mode} to write param"
+  end
+end
+
+def read_next(input, index, intake)
+  puts input[index..(index + intake - 1)].join(' ') if ENV['DEBUG']
+end
+
+def step(index, input, params)
+  relative_base = params[:relative_base]
+  stdin = params[:stdin]
+  stdout = params[:stdout]
+  opcode = input[index] % 100
+  modes = (input[index] / 100).digits + [0] * 3 # extend modes with enough 0s
+  read_next(input, index, 8)
+  print "Opcode is #{opcode} " if ENV['DEBUG']
+  case opcode
+  when 1
+    debug 'addition'
+
+    old_value = input[input[index + 3]]
+    value = read_param(modes[0], input, index + 1, relative_base) + read_param(modes[1], input, index + 2, relative_base)
+    write_param(modes[2], input, index+3, relative_base, value)
+    # puts input.join(",") if ENV['DEBUG']
+    4
+  when 2
+    debug 'multiplication'
+
+    value = read_param(modes[0], input, index + 1, relative_base) * read_param(modes[1], input, index + 2, relative_base)
+    write_param(modes[2], input, index+3, relative_base, value)
+    # puts input.join(",") if ENV['DEBUG']
+    4
+  when 3
+    debug 'stdin'
+    raise 'Already consumed stdin' if stdin.empty? && stdin.is_a?(Array)
+
+    old_value = input[input[index + 1]]
+    write_param(modes[0], input, index+1, relative_base, stdin.pop)
+    # puts input.join(",") if ENV['DEBUG']
+    2
+  when 4
+    debug 'stdout'
+    stdout << read_param(modes[0], input, index + 1, relative_base)
+    #debug "STDOUT: #{stdout.join(',')}"
+    2
+  when 5
+    debug 'jump-if-true'
+    if read_param(modes[0], input, index + 1, relative_base) != 0
+      puts "Jumping to #{read_param(modes[1], input, index + 2, relative_base)}" if ENV['DEBUG']
+      # puts input.join(",") if ENV['DEBUG']
+      read_param(modes[1], input, index + 2, relative_base) - index
+    else
+      puts 'Not jumping' if ENV['DEBUG']
+      3
+    end
+  when 6
+    debug 'jump-if-false'
+    if read_param(modes[0], input, index + 1, relative_base) == 0
+      puts "Jumping to #{read_param(modes[1], input, index + 2, relative_base)}" if ENV['DEBUG']
+      # puts input.join(",") if ENV['DEBUG']
+      read_param(modes[1], input, index + 2, relative_base) - index
+    else
+      puts 'Not jumping' if ENV['DEBUG']
+      3
+    end
+  when 7
+    debug 'lower-than'
+
+    a = read_param(modes[0], input, index + 1, relative_base)
+    b = read_param(modes[1], input, index + 2, relative_base)
+    value = a < b ? 1 : 0
+    write_param(modes[2], input, index+3, relative_base, value)
+    # puts input.join(",") if ENV['DEBUG']
+    # puts input.join(",") if ENV['DEBUG']
+    4
+  when 8
+    debug 'equal'
+
+    a = read_param(modes[0], input, index + 1, relative_base)
+    b = read_param(modes[1], input, index + 2, relative_base)
+    value = a == b ? 1 : 0
+    write_param(modes[2], input, index+3, relative_base, value)
+    # puts input.join(",") if ENV['DEBUG']
+    4
+  when 9
+    debug 'change of relative base offset'
+
+    params[:relative_base] += read_param(modes[0], input, index + 1, relative_base)
+    2
+  when 99
+    debug 'Halt!' if ENV['DEBUG']
+    # puts input.join(",") if ENV['DEBUG']
+    false
+  else
+    raise "Error with unknown opcode #{opcode} at #{index}"
+  end
+end
+
+class ExtensibleArray < Array
+  def initialize(array)
+    super()
+    array.each_with_index do |el,index|
+      self[index] = el
+    end
+  end
+
+  def [](a)
+    case a
+    when Integer
+      custom_extend(a) if a >= self.size
+    when Range
+      custom_extend(a.end) if a.end >= self.size
+    end
+    super
+  end
+
+  def []=(a,b)
+    custom_extend(a) if a >= self.size
+    super
+  end
+
+  def custom_extend(index)
+    debug "Extend array"
+    while index > self.size / 1.5
+      self.push(0)
+    end
+  end
+end
+
+def iterate(input, stdin: [], stdout: [])
+  params = { stdin: stdin, stdout: stdout, relative_base: 0 }
+  # puts input.join(",") if ENV['DEBUG']
+  i = 0
+  increment = 0
+  input = ExtensibleArray.new(input)
+  while (increment = step(i, input, params))
+    puts "Moving cursor from #{i} to #{i + increment}" if ENV['DEBUG']
+    i += increment
+  end
+  input
+end
+
+class HullRobot
+  attr_reader :hull
+  def initialize(program)
+    @program = program
+    @location = [0,0]
+    @direction = 0 # 0 up, 1: right, 2: down, 3: left
+    @hull = Hash.new(0)
+  end
+
+  def run
+    input = Stdin.new(self)
+    stdout = Stdout.new(self)
+    iterate(@program, stdin: input, stdout: stdout)
+    stdout
+  end
+
+  def read_input
+    @hull[@location]
+  end
+
+  def color(new_color)
+    debug2 "Coloring #{@location.join(',')} in #{new_color}"
+    @hull[@location] = new_color
+  end
+
+  def rotate(new_direction)
+    case new_direction
+    when 0
+      @direction = (@direction - 1) % 4
+    when 1
+      @direction = (@direction + 1) % 4
+    else
+      raise "Unknown direction #{direction}"
+    end
+    #now move
+    case @direction
+    when 0
+      @location = [@location[0], @location[1]-1]
+    when 1
+      @location = [@location[0]+1, @location[1]]
+    when 2
+      @location = [@location[0], @location[1]+1]
+    when 3
+      @location = [@location[0]-1, @location[1]]
+    end
+    debug2 "Rotation #{new_direction}, moving to #{@location.join(',')}"
+  end
+end
+
+class Stdin
+  def initialize(robot)
+    @robot = robot
+  end
+
+  def pop
+    @robot.read_input
+  end
+
+  def empty?
+    false
+  end
+end
+
+class Stdout
+  def initialize(robot)
+    @robot = robot
+    @color = true # true when waiting for color, false when waiting for rotation
+  end
+  def <<(el)
+    if @color
+      @robot.color(el)
+    else
+      @robot.rotate(el)
+    end
+    @color = !@color
+  end
+end
+
+robot = HullRobot.new(input_copy)
+robot.run
+
+puts "First part: #{robot.hull.size}"
+
+
+__END__
+3,8,1005,8,338,1106,0,11,0,0,0,104,1,104,0,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,1,8,10,4,10,102,1,8,28,1,108,6,10,1,3,7,10,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,1,8,10,4,10,1001,8,0,58,2,5,19,10,1,1008,7,10,2,105,6,10,1,1007,7,10,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,0,10,4,10,101,0,8,97,1006,0,76,1,106,14,10,2,9,9,10,1006,0,74,3,8,102,-1,8,10,101,1,10,10,4,10,108,1,8,10,4,10,1002,8,1,132,1006,0,0,2,1104,15,10,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,0,10,4,10,1001,8,0,162,1,1005,13,10,3,8,1002,8,-1,10,101,1,10,10,4,10,108,1,8,10,4,10,101,0,8,187,1,1,15,10,2,3,9,10,1006,0,54,3,8,102,-1,8,10,101,1,10,10,4,10,108,0,8,10,4,10,102,1,8,220,1,104,5,10,3,8,102,-1,8,10,101,1,10,10,4,10,1008,8,0,10,4,10,102,1,8,247,1,5,1,10,1,1109,2,10,3,8,1002,8,-1,10,101,1,10,10,4,10,1008,8,0,10,4,10,1001,8,0,277,1006,0,18,3,8,1002,8,-1,10,101,1,10,10,4,10,108,1,8,10,4,10,101,0,8,301,2,105,14,10,1,5,1,10,2,1009,6,10,1,3,0,10,101,1,9,9,1007,9,1054,10,1005,10,15,99,109,660,104,0,104,1,21101,0,47677546524,1,21101,0,355,0,1105,1,459,21102,936995299356,1,1,21101,0,366,0,1106,0,459,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,21101,0,206312807515,1,21102,1,413,0,1105,1,459,21101,206253871296,0,1,21102,424,1,0,1106,0,459,3,10,104,0,104,0,3,10,104,0,104,0,21102,1,709580554600,1,21102,1,447,0,1105,1,459,21101,0,868401967464,1,21101,458,0,0,1106,0,459,99,109,2,22102,1,-1,1,21102,1,40,2,21101,0,490,3,21102,480,1,0,1106,0,523,109,-2,2105,1,0,0,1,0,0,1,109,2,3,10,204,-1,1001,485,486,501,4,0,1001,485,1,485,108,4,485,10,1006,10,517,1101,0,0,485,109,-2,2105,1,0,0,109,4,2101,0,-1,522,1207,-3,0,10,1006,10,540,21102,0,1,-3,21201,-3,0,1,21202,-2,1,2,21101,0,1,3,21101,0,559,0,1105,1,564,109,-4,2106,0,0,109,5,1207,-3,1,10,1006,10,587,2207,-4,-2,10,1006,10,587,21202,-4,1,-4,1105,1,655,21201,-4,0,1,21201,-3,-1,2,21202,-2,2,3,21102,606,1,0,1105,1,564,22102,1,1,-4,21102,1,1,-1,2207,-4,-2,10,1006,10,625,21102,1,0,-1,22202,-2,-1,-2,2107,0,-3,10,1006,10,647,22101,0,-1,1,21101,0,647,0,106,0,522,21202,-2,-1,-2,22201,-4,-2,-4,109,-5,2106,0,0
