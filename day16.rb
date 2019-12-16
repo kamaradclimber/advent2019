9c010a6e49e8bb74cc78233b5771a4fd2991c4ef
Grégoire Seux <g.seux@criteo.com>
2019-12-16 10:19:00 +0100

    Second part day16 for good
    63s to compute all examples and parts
    
    Change-Id: Ib02beb6f1c612512ce0f30669e85dd4c090f4a56

--- day16.rb
+++ day16.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+#
+require 'pry'
 
 def debug(s)
   puts s if ENV['DEBUG']
@@ -89,7 +91,37 @@ puts 'Test done'
 my_input = "59749012692497360857047467554796667139266971954850723493205431123165826080869880148759586023048633544809012786925668551639957826693687972765764376332866751104713940802555266397289142675218037547096634248307260671899871548313611756525442595930474639845266351381011380170903695947253040751698337167594183713030695951696818950161554630424143678657203441568388368475123279525515262317457621810448095399401556723919452322199989580496608898965260381604485810760050252737127446449982328185199195622241930968611065497781555948187157418339240689789877555108205275435831320988202725926797165070246910002881692028525921087207954194585166525017611171911807360959"
 puts "First part #{phase(my_input,100)}"
 
-long_input = my_input.chars.map(&:to_i) * 10000
+def next_phase(input)
+  i = input.size - 1
+  next_input = [0] * input.size
+  (i / 2).times do |j|
+    index = i - j
+    previous = if index + 1 == input.size
+                 0
+               else
+                 next_input[index+1]
+               end
+    next_input[index] = (previous + input[index]).digits.first
+  end
+  next_input
+end
 
-ENV['DEBUG'] = ''
-puts "Second part: #{phase(long_input, 100)}"
+def partial_phase(input, phase_count)
+  current_phase = if input.is_a?(String)
+                    input_signal(input)
+                  else
+                    input
+                  end
+  offset = current_phase.take(7).join.to_i
+  phase_count.times do |phase_id|
+    debug phase_id
+    current_phase = next_phase(current_phase)
+  end
+  
+  current_phase.drop(offset).take(8)
+end
+
+raise "Failed example5" unless 84462026 == partial_phase("03036732577212944063491565474664".chars.map(&:to_i) * 10000, 100).join.to_i
+raise "Failed example6" unless 78725270 == partial_phase("02935109699940807407585447034323".chars.map(&:to_i) * 10000, 100).join.to_i
+raise "Failed example7" unless 53553731 == partial_phase("03081770884921959731165446850517".chars.map(&:to_i) * 10000, 100).join.to_i
+puts "Second part: #{partial_phase(my_input.chars.map(&:to_i) * 10000, 100).join}"
