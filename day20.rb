1c329e8d3d577a152795cfaeef98a43456e8940b
Grégoire Seux <g.seux@criteo.com>
2019-12-20 07:53:03 +0100

    WIP day20
    Change-Id: I1d7af5ec97db9bc479bdeb04ed3db08e972053d2

--- day20.rb
+++ day20.rb
@@ -345,7 +345,8 @@ def outer_to_inner_distances(maze, portals)
   distance_from_AA = update_distances_no_portal(maze, portals['AA'].first)
   meta_distances[['AA', 'ZZ']] = distance_from_AA[portals['ZZ'].first]
   portals.each do |dest, point|
-    meta_distances[['AA', dest]] = distance_from_AA[dest]
+    inner = coords.min_by { |point| distance_to_maze_center(point, maze) }
+    meta_distances[['AA', dest]] = distance_from_AA[inner]
   end
   meta_distances
 end
