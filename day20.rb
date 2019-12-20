4a9d3b79fd62f49e8091acbc08ae6fbdbd350baa
Grégoire Seux <g.seux@criteo.com>
2019-12-20 13:10:27 +0100

    WIP day20
    Change-Id: Ie210a653419e383ce2837eedfe8ed6df27771d1d

--- day20.rb
+++ day20.rb
@@ -429,16 +429,18 @@ def update_distances_graph(graph, starting_point)
       d = graph[[point,candidate]] || graph[[candidate,point]]
       best_dist = [current_dist, distances[point] + d].compact.min
       distances[candidate + 'x' * inception] = best_dist
-      debug "Best distance between #{candidate} (#{inception}) and #{starting_point} is (for now) #{distances[candidate + 'x' * inception]}"
+      debug "Best distance between #{candidate} (#{inception}) and #{starting_point} is (for now) #{distances[candidate + 'x' * inception]}" unless distances[candidate + 'x' *inception] == distances.default
       if candidate.gsub(/.+_/, '') == point.gsub(/.+_/, '')
         case candidate
         when /^inner_/
           if inception > 0
+            debug "Moving upper"
             neighbours_graph[candidate].each do |new_candidate|
               visited[new_candidate + 'x' * (inception-1)] ||= false
             end
           end
         when /^outer_/
+          debug "Moving lower"
           neighbours_graph[candidate].each do |new_candidate|
             visited[new_candidate + 'x' * (inception+1)] ||= false
           end
