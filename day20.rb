33a61f881f3820deab3cb3be9518883859e7a01b
Grégoire Seux <g.seux@criteo.com>
2019-12-20 17:56:22 +0100

    WIP day20
    Change-Id: I1c1178df018d810133fbcc6f77c8354871a8f8d7

--- day20.rb
+++ day20.rb
@@ -443,15 +443,15 @@ def update_distances_graph(graph, starting_point)
         end
       end
       if new_inception < 0
-        debug "Skipping #{point} because it does not exist at this inception level"
+        debug "  Skipping #{candidate} because it does not exist at this inception level"
         next
       end
       current_dist = distances[candidate]
       d = graph[[point,candidate]] || graph[[candidate,point]]
       best_dist = [current_dist, distances[full_point_name] + d].compact.min
       distances[candidate + 'x' * new_inception] = best_dist
-      debug "Best distance between #{candidate} (#{new_inception}) and #{starting_point} is (for now) #{distances[candidate + 'x' * new_inception]}" unless distances[candidate + 'x' *new_inception] == distances.default
-      debug "Adding #{candidate + 'x' * (new_inception)} as non visited (unless it was already visited)"
+      debug "  Best distance between #{candidate} (#{new_inception}) and #{starting_point} is (for now) #{distances[candidate + 'x' * new_inception]}" unless distances[candidate + 'x' *new_inception] == distances.default
+      debug "  Adding #{candidate + 'x' * (new_inception)} as non visited (unless it was already visited)" unless visited.key?(candidate + 'x' * (new_inception))
       visited[candidate + 'x' * (new_inception)] ||= false
     end
     remaining_to_explore = distances.select { |point, d| visited[point] == false }
